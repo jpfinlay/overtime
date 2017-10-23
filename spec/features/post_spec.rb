91ea6c0c6d70c3cafadbba39035b1e18de4b2a24
Jamie Finlay <jpfinlay@gmail.com>
2017-10-23 19:34:15 +0100

    Add functionality to create and show a new Post

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -16,6 +16,20 @@ describe "navigate" do
       visit posts_path
       expect(page).to have_content(/Posts/)
     end
+  end
 
+  describe "create" do
+    it "has a new form that can be reached" do
+      visit new_post_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it "accepts a date and rationale and submits the form" do
+      visit new_post_path
+      fill_in "post[date]", with: Date.today
+      fill_in "post[rationale]", with: "A good rationale"
+      click_on "Save"
+      expect(page).to have_content("A good rationale")
+    end
   end
 end
