b93900d90fc222503b677fda1c0ebf7d2c4b3507
Jamie Finlay <jpfinlay@gmail.com>
2017-11-08 14:41:40 +0000

    Add #full_name method for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :first_name, :last_name, presence: true
+
+
+  def full_name
+    last_name.upcase + ", " + first_name.upcase
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe do
-    before do
-      @user = FactoryBot.create :user
-    end
+  before do
+    @user = FactoryBot.create :user
+  end
 
+  describe "A user" do
     it "can be created" do
       expect(@user).to be_valid
     end
@@ -16,6 +16,12 @@ RSpec.describe User, type: :model do
       @user.last_name = nil
 
       expect(@user).to_not be_valid
+    end
+  end
+
+  describe "custom name methods" do
+    it "has a full name method that combines first and last names" do
+      expect(@user.full_name).to eq("SNOW, JON")
     end
   end
 end
