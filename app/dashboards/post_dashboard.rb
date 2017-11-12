601311e6cf61f2f8a9255a8bfec8dac6687140dc
Jamie Finlay <jpfinlay@gmail.com>
2017-11-12 20:25:49 +0000

    Enable searching Posts by Rationale in admin interface

--- app/dashboards/post_dashboard.rb
+++ app/dashboards/post_dashboard.rb
@@ -8,12 +8,12 @@ class PostDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    user: Field::BelongsTo,
-    id: Field::Number,
-    date: Field::DateTime,
-    rationale: Field::Text,
-    created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    user: Field::BelongsTo.with_options(searchable: false),
+    id: Field::Number.with_options(searchable: false),
+    date: Field::DateTime.with_options(searchable: false),
+    rationale: Field::Text.with_options(searchable: true),
+    created_at: Field::DateTime.with_options(searchable: false),
+    updated_at: Field::DateTime.with_options(searchable: false),
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -22,9 +22,9 @@ class PostDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :user,
-    :id,
     :date,
+    :user,
+#    :id,
     :rationale,
   ].freeze
 
